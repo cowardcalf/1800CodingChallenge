d324282cdd43d3ffd09d7734780fb6f832a57454
cowardcalf <cowardcalf@gmail.com>
2016-10-26 00:27:31 +1100

    Add InputTest with test of readArguments method. Add readArguments method into Input.

--- src/main/yangliu/challenge/Input.java
+++ src/main/yangliu/challenge/Input.java
@@ -2,8 +2,35 @@ package main.yangliu.challenge;
 
 public class Input {
 	
-	public static String readPhoneNumberFromArgument(String[] args){
-		return null;
+	public static String[] readArguments(String[] args){
+		String[] result = null;
+		if(args != null){
+			result = new String[]{null, null};
+			boolean hasN = false; // don't check -n if already found
+			boolean hasD = false; // don't check -d if already found
+			for(int i=0; i < args.length; i++){
+				String s = args[i];
+				// have parameter -n or -N
+				if(!hasN && (s.equals("-n") || s.equals("-N"))){
+					hasN = true;
+					if(i + 1 < args.length){
+						String phoneNumber = args[i+1];
+						// Remove all non-numbers
+						phoneNumber = phoneNumber.replaceAll("[^0-9]", "");
+						if(phoneNumber.length() > 0)
+							result[0] = phoneNumber;
+						i++;
+					}
+				// have parameter -d or -D
+				}else if(!hasD && (s.equals("-d") || s.equals("-D"))){
+					hasD = true;
+					if(i + 1 < args.length){
+						String dicPath = args[i+1];
+						result[1] = dicPath;
+					}
+				}
+			}
+		}
+		return result;
 	}
-	
 }
--- src/test/yangliu/challenge/InputTest.java
+++ src/test/yangliu/challenge/InputTest.java
@@ -10,53 +10,60 @@ import main.yangliu.challenge.Input;
 public class InputTest {
 
 	@Test
-	public void whenNoArgumentsReturnNull() {
-		assertNull(Input.readPhoneNumberFromArgument(null));
+	public void ReadArgumentsWhenNoArgumentsReturnNull() {
+		assertNull(Input.readArguments(null));
 	}
 
 	@Test
-	public void whenNoDashNArgumentsReturnNull() {
-		String[] emptyArgs = {""};
-		String[] otherArgs = {"-t", "otherArg"};
-		assertNull("empty arguments", Input.readPhoneNumberFromArgument(emptyArgs));
-		assertNull("have other arguments", Input.readPhoneNumberFromArgument(otherArgs));
+	public void ReadArgumentsWhenNoValidDashFlagReturnStringArrayWithNullValues() {
+		String[] emptyArgs = { "" };
+		String[] otherArgs = { "-t", "otherArg" };
+		String[] result = Input.readArguments(emptyArgs);
+		assertNull("Empty arguments result[0]", result[0]);
+		assertNull("Empty arguments result[1]", result[1]);
+		result = Input.readArguments(otherArgs);
+		assertNull("Other arguments result[0]", result[0]);
+		assertNull("Other arguments result[1]", result[1]);
+	}
+
+	@Test
+	public void ReadArgumentsWhenHasValidDashFlagButNoValidValueReturnStringArrayWithNullValue() {
+		String[] emptyArgsN = { "-n", "" };
+		String[] noFollowArgsN = { "-n" };
+		String[] invalidArgsN = { "-n", "-t" };
+		String[] notNumberArgsN = { "-n", "  a.bc-d e " };
+
+		String[] result = Input.readArguments(emptyArgsN);
+		assertNull("Empty arguments of -n", result[0]);
+		result = Input.readArguments(noFollowArgsN);
+		assertNull("No following arguments of -n", result[0]);
+		result = Input.readArguments(invalidArgsN);
+		assertNull("Invalid arguments of -n", result[0]);
+		result = Input.readArguments(notNumberArgsN);
+		assertNull("No numbers of -n", result[0]);
+		
+		String[] noFollowArgD = { "-d" };
+		result = Input.readArguments(noFollowArgD);
+		assertNull("No following arguments of -d", result[1]);
 	}
 	
 	@Test
-	public void whenHasDashNArgumentButNoNumberAfterThatThenThrowInvalidParameterException() {
-		String[] emptyArgs = {"-n", ""};
-		String[] noFollowArgs = {"-n"};
-		String[] invalidArgs = {"-n", "-t"};
-		String[] notNumberArgs = {"-n", "abcde"};
+	public void ReadArgumentsWhenHasValidDashNFlagWithValidValueReturnNumberInTheFirstItemOfStringArray(){
+		String[] normalNumber = {"-n", "123456"};
+		String[] withWhiteSpace = {"-n", " 1234 56 "};
+		String[] withPunctuation = {"-n", "~!@#1234%^&*5678()-=90[];'"};
+		String[] withLetters = {"-n", "eij12ixcl3456twyo"};
 		
-		RuntimeException exception = null;
-		try{
-			Input.readPhoneNumberFromArgument(emptyArgs);
-		}catch(RuntimeException e){
-			exception = e;
-		}
-		assertNotNull("Empty argument, exception was not thrown", exception);
-		assertEquals("Empty parameter", "Invalid Parameter of -n", exception.getMessage());
-		try{
-			Input.readPhoneNumberFromArgument(noFollowArgs);
-		}catch(RuntimeException e){
-			exception = e;
-		}
-		assertNotNull("No following argument, exception was not thrown", exception);
-		assertEquals("No following argument", "Invalid Parameter of -n", exception.getMessage());
-		try{
-			Input.readPhoneNumberFromArgument(invalidArgs);
-		}catch(RuntimeException e){
-			exception = e;
-		}
-		assertNotNull("Invalid argument, exception was not thrown", exception);
-		assertEquals("Invalid argument", "Invalid Parameter of -n", exception.getMessage());
-		try{
-			Input.readPhoneNumberFromArgument(notNumberArgs);
-		}catch(RuntimeException e){
-			exception = e;
-		}
-		assertNotNull("Not number argument, exception was not thrown", exception);
-		assertEquals("Not number argument", "Invalid Parameter of -n", exception.getMessage());
+		assertEquals("Normal numbers", "123456", Input.readArguments(normalNumber)[0]);
+		assertEquals("With white spaces", "123456", Input.readArguments(withWhiteSpace)[0]);
+		assertEquals("With punctuations", "1234567890", Input.readArguments(withPunctuation)[0]);
+		assertEquals("With Letters", "123456", Input.readArguments(withLetters)[0]);
+	}
+	
+	@Test
+	public void ReadArgumentsWhenHasValidDashDFlagWithValidValueReturnTheStringInTheSecondItemOfStringArray(){
+		String[] args = {"-d", "/document/dictionary.txt"};
+		
+		assertEquals("Normal value", "/document/dictionary.txt", Input.readArguments(args)[1]);
 	}
 }
